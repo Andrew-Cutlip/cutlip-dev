291214d218c6d1624426b0a622900218a4c942f3
Andrew-Cutlip <snakeeye10@gmail.com>
2019-10-05 01:14:50 -0400

    adds about page, contact page, and footer

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const footer = ({siteTitle}) => {
+    <footer>
+        {siteTitle}
+    </footer>
+}
+export default footer
